09504f80cd6f442cf1f522f4210ebadb3979c402
SimonEvans1969 <simon@nautilus-consulting.co.uk>
2024-01-06 18:07:18 +0100

    Revert to old app.layout.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,6 @@
 import './bootstrap';
 import Alpine from 'alpinejs';
 import jQuery from 'jquery';
-import DataTable from 'datatables.net-dt';
-import 'datatables.net-buttons-dt';
-import 'datatables.net-fixedcolumns-dt';
 
 window.Alpine = Alpine;
 Alpine.start();
