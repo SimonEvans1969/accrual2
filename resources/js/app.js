776e17f2167d1498043590cb5964a0efd0a26b74
SimonEvans1969 <simon@nautilus-consulting.co.uk>
2024-01-06 18:17:10 +0100

    Revert "Added Datatables import to app.js"
    This reverts commit c10b466610cf9af1b62a7ca7341500ca87ab611e.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,10 @@
 import './bootstrap';
+
 import Alpine from 'alpinejs';
-import jQuery from 'jquery';
-import DataTable from 'datatables.net-dt';
-import 'datatables.net-buttons-dt';
-import 'datatables.net-fixedcolumns-dt';
 
 window.Alpine = Alpine;
+
 Alpine.start();
 
+import jQuery from 'jquery';
 window.$ = jQuery;
-
-
