db7a10311a8c84c8f0c3ef8d4910d9a038988a6f
SimonEvans1969 <simon@nautilus-consulting.co.uk>
2024-01-08 18:02:42 +0000

    Another go at importing jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
-import jQuery from 'jquery';
+import $ from 'jquery';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.start();
 
-window.$ = jQuery;
+window.jQuery = window.$ = $;
